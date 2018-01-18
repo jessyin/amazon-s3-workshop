9dc3952feee0e4d130aaf01f5568a24061ef5fe9
Alex Chen <achen19@mit.edu>
2018-01-18 03:08:03 -0500

    first working aws

---
+++ js/photo.js
@@ -0,0 +1,107 @@
+var albumBucketName = 'alex-chen-cool-photos';
+var bucketRegion = 'us-east-1';
+var IdentityPoolId = 'us-east-1:20c8e41f-fd64-4051-8c70-05939a1e4831';
+
+AWS.config.update({
+  region: bucketRegion,
+  credentials: new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: IdentityPoolId
+  })
+});
+
+var s3 = new AWS.S3({
+  apiVersion: '2006-03-01',
+  params: {Bucket: albumBucketName}
+});
+function getHtml(template) {
+  return template.join('\n');
+}
+
+function viewAlbum() {
+  //var albumPhotosKey = encodeURIComponent(albumName);
+  s3.listObjects({}, function(err, data) {
+    if (err) {
+      return alert('There was an error viewing your album: ' + err.message);
+    }
+    // `this` references the AWS.Response instance that represents the response
+    var href = this.request.httpRequest.endpoint.href;
+    var bucketUrl = href + albumBucketName + '/';
+
+    var photos = data.Contents.map(function(photo) {
+      var photoKey = photo.Key;
+      var photoUrl = bucketUrl + encodeURIComponent(photoKey);
+      return getHtml([
+        '<span>',
+          '<div>',
+            '<img src="' + photoUrl + '"/>',
+          '</div>',
+          '<div>',
+            '<span onclick="deletePhoto(\'' + photoKey + '\')">',
+              'X',
+            '</span>',
+            '<span>',
+              photoKey,
+            '</span>',
+          '</div>',
+        '<span>',
+      ]);
+    });
+    var message = photos.length ?
+      '<p>Click on the X to delete the photo</p>' :
+      '<p>You do not have any photos in this album. Please add photos.</p>';
+    var htmlTemplate = [
+      '<h2>',
+        'Album: ',
+      '</h2>',
+      message,
+      '<div>',
+        getHtml(photos),
+      '</div>',
+      '<input id="photoupload" type="file" accept="image/*">',
+      '<button id="addphoto" onclick="addPhoto()">',
+        'Add Photo',
+      '</button>',
+      '<button onclick="listAlbums()">',
+        'Back To Albums',
+      '</button>',      
+    ]
+    document.getElementById('app').innerHTML = getHtml(htmlTemplate);
+  });
+}
+
+function addPhoto(albumName) {
+  var files = document.getElementById('photoupload').files;
+  if (!files.length) {
+    return alert('Please choose a file to upload first.');
+  }
+  var file = files[0];
+  var fileName = file.name;
+  var albumPhotosKey = encodeURIComponent(albumName) ;
+
+  var photoKey = albumPhotosKey + fileName;
+  s3.upload({
+    Key: photoKey,
+    Body: file,
+    ACL: 'public-read'
+  }, function(err, data) {
+    if (err) {
+      return alert('There was an error uploading your photo: ', err.message);
+    }
+    alert('Successfully uploaded photo.');
+    viewAlbum();
+  });
+}
+
+function deletePhoto(photoKey) {
+  s3.deleteObject({Key: photoKey}, function(err, data) {
+    if (err) {
+      return alert('There was an error deleting your photo: ', err.message);
+    }
+    alert('Successfully deleted photo.');
+    viewAlbum();
+  });
+}
+
+
+
+
